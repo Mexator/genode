d737ca46b6ba765871a2c9571b72ff7bb43e021f
Anton Brisilin <a.brisilin@innopolis.university>
2022-02-24 19:19:16 +0300

    Add a dummy persistent storage to ptcp_test scenario

--- repos/ptcp/src/test/ptcp_test/main.cc
+++ repos/ptcp/src/test/ptcp_test/main.cc
@@ -4,7 +4,9 @@
 // socket api
 #include <netinet/in.h> // in_addr
 #include <sys/socket.h> // socket
-
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
 void test() {
     int sock = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -20,6 +22,20 @@ void test() {
 void Libc::Component::construct(Libc::Env &env) {
     with_libc([&]() {
         puts("hello world");
+        int desc = open("/test/file", O_RDWR | O_CREAT);
+        Genode::log("file opened", desc, " ", errno);
+
+        int r = write(desc, "hello hello!!", 14);
+        fsync(desc);
+        Genode::log("write finished", r, " ", errno);
+
+        char a[125] = "";
+        r = read(desc, a, 125);
+        Genode::log("read finished", r, " ", errno, " ");
+        puts(a);
+        puts("a");
+        puts(a);
+
         test();
     });
 }
